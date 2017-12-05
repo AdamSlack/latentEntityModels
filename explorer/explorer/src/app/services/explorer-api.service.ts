b373d5db39152e0a652cf552cd197f690c78570d
Adam Slack <adamslack@outlook.com>
2017-12-05 09:49:18 +0000

    changed root of api service

--- explorer/explorer/src/app/services/explorer-api.service.ts
+++ explorer/explorer/src/app/services/explorer-api.service.ts
@@ -8,7 +8,7 @@ export class ExplorerApiService {
 
   constructor(private http: HttpClient) { }
   
-    public ROOT : string = 'http://localhost:8081';
+    public ROOT : string = 'http://localhost:8081/';
   
     public requestEntities(book_title: string) : Observable<any> {
       // example... 'http://localhost:8080/stockbroker?stockID=AMG&granularity=TIME_SERIES_DAILY'
