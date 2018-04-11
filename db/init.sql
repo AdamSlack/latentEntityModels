e52fcefec71e0d1f0ee6c09555a3b3c2fbb940c2
Adam Slack <adamslack@outlook.com>
2018-04-11 11:57:01 +0100

    db changes, and some diagrams

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@ begin;
 --  table of books and entity-term associations
 --
 
-create table books(
+create table books( -- Actually book_entity_terms
     id         serial   not null,
     book_title text     not null,
     entity     text     not null,
@@ -31,6 +31,28 @@ create table book_topics(
 drop materialized view if exists book_titles;
 create materialized view book_titles as
     select distinct book_title from books;
+
+create table book_entity_topics(
+    entity     text not null ,
+    book_title text not null ,
+    topic_id   serial not null,
+    strength   float not null,
+    primary key(entity, book_title, topic_id)
+);
+
+create table latent_entities(
+    entity_id    serial  not null,
+    topic_id     serial  not null ,
+    score        float   not null,    
+    primary key(entity_id, topic_id)
+);
+
+create table latent_entity_entities(
+    entity_id   serial not null,
+    entity      text   not null,
+    euclid_dist float,
+    primary key (entity_id, entity)
+);
 
 
 commit;
