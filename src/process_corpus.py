b8c8a5c03f7e14eb7c3873fd2871517014650b02
Adam Slack <adamslack@outlook.com>
2018-04-18 17:31:35 +0100

    changes

--- src/process_corpus.py
+++ src/process_corpus.py
@@ -27,7 +27,7 @@ def main():
     """ Main """
     conn = db.connect_to_db(host='localhost', dbname='books', user='postgres', password='password')
 
-    dir_path = '../test_data/'
+    dir_path = '../test_data/harry_potter/'
     glob_str = '*.txt'
     print('Begginging Process of assimilation')
     for fp in glob.glob(dir_path + glob_str):
