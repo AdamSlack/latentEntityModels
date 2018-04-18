e823072be6693c413b3a59c17fe210278511028e
Adam Slack <adamslack@outlook.com>
2018-04-18 14:39:34 +0100

    some R crap

--- R/scatter_matrix.R
+++ R/scatter_matrix.R
@@ -1,8 +1,12 @@
 library(readr)
 library(lattice)
-
+library(ggplot2)
+library(GGally)
 e_class = read_csv("~/github/latententitymodels/results/entity_classification.csv")
 e_class[1] = NULL
 
 e_class[c("class")] <- lapply(e_class[c("class")], function(x) x+1)
-pairs(e_class[1:10], col=e_class$class)
+
+pairs(e_class[1:10], col=e_class$class, pch=4)
+
+
