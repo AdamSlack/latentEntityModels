71e1b78e2bb3186279bde210b2c7ed77920f9823
Adam Slack <adamslack@outlook.com>
2018-04-21 10:41:11 +0100

    main LE discussion done.

--- R/scatter_matrix.R
+++ R/scatter_matrix.R
@@ -7,6 +7,7 @@ e_class[1] = NULL
 
 e_class[c("class")] <- lapply(e_class[c("class")], function(x) x+1)
 
-pairs(e_class[1:10], pch=4, cex.labels=2, col=e_class$class)
-
-
+pairs(e_class[1:10], pch=4, cex.labels=2, col=e_class$class, oma=c(4,4,20,20))
+par(xpd = TRUE)
+legend( "topright", fill = unique(c("1","2","3","4","5")), 
+                       legend = c( unique(e_class$class) ) )
